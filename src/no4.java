8bb6d4d3492eeb3f9b42e00037f65555e5a7a486
aleksandrzh52 <aleksandr.zh52@gmail.com>
2021-12-23 20:06:36 +0300

    array

--- src/no4.java
+++ src/no4.java
@@ -1,6 +1,16 @@
 public class no4 {
     public static void main(String[] args) {
-        System.out.printf("Hello");
+        //Напишите программу, в которой будет массив,
+        // заполненный элементами от 5 до 15, и напечатайте все элементы массива
+
+        int[] arr = new int[11];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i+5;
+            System.out.println(arr[i]);
+
+        }
+
+
     }
 
 }
