fd8ef15f7505a671d3c5b41ea947859f01640239
aleksandrzh52 <aleksandr.zh52@gmail.com>
2021-12-25 20:17:37 +0300

    array

--- src/no4.java
+++ src/no4.java
@@ -1,16 +1,24 @@
 public class no4 {
+    static class Name{
+        public void checkUserName(String name1, String name2){
+            if(name1.equalsIgnoreCase(name2)){
+                System.out.print("Выберите другого пользователя");
+            } else {
+                System.out.print("Отличный выбор!\nВаше имя имеет длину " + name2.length() + " символов");
+                System.out.println("\nА без пробелов ваше имя занимает " + (name2.replaceAll(" ","")).length() + " символов");
+            }
+        }
+    }
     public static void main(String[] args) {
-        //Напишите программу, в которой будет массив,
-        // заполненный элементами от 5 до 15, и напечатайте все элементы массива
+        Name name = new Name();
+        String name1 = "Иван Иванов";
+        String name2 = "Вован Иванов Иванович";
+        name.checkUserName(name1,name2);
 
-        int[] arr = new int[11];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]=i+5;
-            System.out.println(arr[i]);
+
+
 
         }
-
-
     }
 
-}
+
---
+++ src/prinNo2.java
@@ -0,0 +1,2 @@
+public class prinNo2 {
+}
